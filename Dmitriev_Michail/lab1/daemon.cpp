3a57accecc38030b588b473d07cae5e2b290e7e8
Dvimin <dmitriev.ml@edu.spbstu.ru>
2024-10-28 18:24:41 +0300

    Moved the files to the /Dmitriev_Michail/lab1

---
+++ Dmitriev_Michail/lab1/daemon.cpp
@@ -0,0 +1,139 @@
+#include "daemon.h"
+
+#include <cstring>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void Daemon::signal_handler(int sig) {
+    switch (sig) {
+        case SIGHUP:
+            got_sighup = true;
+            syslog(LOG_INFO, "Получен сигнал SIGHUP для повторного чтения конфигурации");
+            break;
+        case SIGTERM:
+            got_sigterm = true;
+            syslog(LOG_INFO, "Получен сигнал SIGTERM, завершение работы");
+            break;
+    }
+}
+
+void Daemon::start(const std::string& config_file) {
+    current_path = std::filesystem::absolute(config_file).parent_path().string();
+    daemonize();
+    open_config_file(config_file);
+    run(config_file);
+}
+
+void Daemon::create_pid_file() {
+    std::string pid_file = "/var/run/daemon_example.pid";
+    int pid_file_handle = open(pid_file.c_str(), O_RDWR | O_CREAT, 0600);
+
+    if (pid_file_handle == -1) {
+        syslog(LOG_ERR, "Не удалось открыть PID файл %s", pid_file.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    if (lockf(pid_file_handle, F_TLOCK, 0) == -1) {
+        syslog(LOG_ERR, "Демон уже запущен (PID файл заблокирован)");
+        exit(EXIT_FAILURE);
+    }
+
+    char old_pid_str[10];
+    if (read(pid_file_handle, old_pid_str, sizeof(old_pid_str) - 1) > 0) {
+        int old_pid = atoi(old_pid_str);
+        if (old_pid > 0 && kill(old_pid, 0) == 0) {
+            syslog(LOG_INFO, "Процесс с PID %d уже запущен, отправка SIGTERM", old_pid);
+            kill(old_pid, SIGTERM);
+            sleep(1);
+        } else {
+            syslog(LOG_INFO, "Не найден процесс с PID %d, продолжаем...", old_pid);
+        }
+    }
+
+    ftruncate(pid_file_handle, 0);
+    lseek(pid_file_handle, 0, SEEK_SET);
+    snprintf(old_pid_str, sizeof(old_pid_str), "%d\n", getpid());
+    write(pid_file_handle, old_pid_str, strlen(old_pid_str));
+    syslog(LOG_INFO, "PID файл %s успешно создан с PID %d", pid_file.c_str(), getpid());
+    close(pid_file_handle);
+}
+
+void Daemon::daemonize() {
+    if (fork() > 0) exit(EXIT_SUCCESS);
+
+    umask(0);
+    setsid();
+    chdir("/");
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    create_pid_file();
+}
+
+void Daemon::open_config_file(const std::string& config_file) {
+    std::ifstream infile(config_file);
+    std::string line;
+    table.clear();
+    time_points.clear();
+
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        Data data;
+        if (iss >> data.folder1 >> data.folder2 >> data.ext) {
+            data.interval = 20;
+            table.push_back(data);
+            time_points.emplace_back(std::chrono::steady_clock::now());
+        } else {
+            syslog(LOG_WARNING, "Ошибка при чтении строки конфигурации: %s", line.c_str());
+        }
+    }
+}
+
+void Daemon::replace_folder(const Data& data) {
+    auto cur_path1 = std::filesystem::path(data.folder1);
+    auto cur_path2 = std::filesystem::path(data.folder2);
+    std::filesystem::path current_path_fs(current_path);
+
+    if (cur_path1.is_relative()) cur_path1 = current_path_fs / data.folder1;
+    if (cur_path2.is_relative()) cur_path2 = current_path_fs / data.folder2;
+
+    std::filesystem::create_directories(cur_path2);
+    for (const auto& entry : std::filesystem::directory_iterator(cur_path1)) {
+        if (entry.is_regular_file() && (data.ext.empty() || entry.path().extension() != "." + data.ext)) {
+            std::filesystem::rename(entry.path(), cur_path2 / entry.path().filename());
+            syslog(LOG_INFO, "Перемещен файл: %s в %s", entry.path().c_str(), cur_path2.c_str());
+        }
+    }
+}
+
+void Daemon::run(const std::string& config_file) {
+    while (true) {
+        if (got_sighup) {
+            got_sighup = false;
+            open_config_file(config_file);
+        }
+
+        if (got_sigterm) {
+            closelog();
+            exit(EXIT_SUCCESS);
+        }
+
+        for (size_t i = 0; i < table.size(); ++i) {
+            auto now_time = std::chrono::steady_clock::now();
+            int diff = std::chrono::duration_cast<std::chrono::seconds>(now_time - time_points[i]).count();
+
+            if (diff >= table[i].interval) {
+                try {
+                    replace_folder(table[i]);
+                    time_points[i] = now_time;
+                } catch (const std::exception& e) {
+                    syslog(LOG_ERR, "Ошибка при перемещении файлов: %s", e.what());
+                }
+            }
+        }
+
+        sleep(1);
+    }
+}
---
+++ Dmitriev_Michail/lab1/daemon.h
@@ -0,0 +1,49 @@
+#ifndef DAEMON_HPP
+#define DAEMON_HPP
+
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <chrono>
+#include <syslog.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <fstream>
+#include <sstream>
+
+struct Data {
+    std::string folder1;
+    std::string folder2;
+    std::string ext;
+    int interval;
+};
+
+class Daemon {
+public:
+    static Daemon& getInstance() {
+        static Daemon instance;
+        return instance;
+    }
+
+    void start(const std::string& config_file);
+
+private:
+    Daemon() : got_sighup(false), got_sigterm(false) {}
+    Daemon(const Daemon&) = delete;
+    Daemon& operator=(const Daemon&) = delete;
+
+    void replace_folder(const Data& data);
+    void open_config_file(const std::string& config_file);
+    void create_pid_file();
+    void daemonize();
+    void run(const std::string& config_file);
+    void signal_handler(int sig);
+
+    bool got_sighup;
+    bool got_sigterm;
+    std::vector<Data> table;
+    std::vector<std::chrono::time_point<std::chrono::steady_clock>> time_points;
+    std::string current_path;
+};
+#endif // DAEMON_HPP
